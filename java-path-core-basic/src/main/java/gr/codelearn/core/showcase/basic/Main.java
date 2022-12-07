771fa94c2286433a52179666db9a50069a0ee1bd
IoannisKlian <giannisklian@gmail.com>
2022-12-07 21:14:28 +0200

    Add a simple example to show date-time

--- java-path-core-basic/src/main/java/gr/codelearn/core/showcase/basic/Main.java
+++ java-path-core-basic/src/main/java/gr/codelearn/core/showcase/basic/Main.java
@@ -3,9 +3,12 @@ package gr.codelearn.core.showcase.basic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.LocalDateTime;
+
 public class Main {
 	private static final Logger logger = LoggerFactory.getLogger(Main.class);
 	public static void main(String[] args) {
 		logger.info("Hello Java Programmers.");
+		logger.info("Current date and time: {}", LocalDateTime.now());
 	}
 }
