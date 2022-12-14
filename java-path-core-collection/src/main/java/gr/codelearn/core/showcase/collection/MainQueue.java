160d12fda04d7fb96bba599713bcd969b17448e5
Ioannis Daniil <jondan97@gmail.com>
2022-12-14 20:26:47 +0200

    added queue class

---
+++ java-path-core-collection/src/main/java/gr/codelearn/core/showcase/collection/MainQueue.java
@@ -0,0 +1,17 @@
+package gr.codelearn.core.showcase.collection;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+
+public class MainQueue {
+	public static void main(String[] args) {
+		Queue<String> queue = new LinkedList<>();
+		queue.peek();
+		queue.poll();
+
+		PriorityQueue<String> queue2 = new PriorityQueue<>();
+	}
+}
