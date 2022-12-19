9b2d29f0189d8a593c01f19df8d1b718df9a348c
Ioannis Daniil <jondan97@gmail.com>
2022-12-19 18:56:30 +0200

    stream create actions finished,

--- java-path-core-stream/src/main/java/gr/codelearn/Main.java
+++ java-path-core-stream/src/main/java/gr/codelearn/Main.java
@@ -5,9 +5,11 @@ import gr.codelearn.model.Transaction;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 @Slf4j
@@ -54,6 +56,47 @@ public class Main {
 												.collect(Collectors.toList());
 											  //.forEach(t -> log.info("{}", t));
 		// @formatter:on
+
+		streamCreateActions();
+
+		log.info("------------------");
+
+		List<String> list1 = List.of("four", "five");
+		List<String> list2 = List.of("one", "one", "one");
+		List<String> list3 = List.of("one", "one", "one");
+
+		Stream.concat(list1.stream(), list2.stream());
+
+		List<List<String>> listOfLists = List.of(list1, list2, list3);
+		List<String> strings = listOfLists.stream().flatMap(l -> l.stream()).collect(Collectors.toList());
 	}
+
+	private static void streamTerminalActions(){
+
+	}
+
+	private static void streamCreateActions() {
+		Stream<String> streamOfList = List.of("one", "one", "one", "two", "three", "four", "five").stream();
+
+		streamOfList.distinct().forEach(log::info);
+
+		//streamOfList.distinct().forEach(log::info); //Exception in thread "main" java.lang.IllegalStateException: stream has already been operated upon or closed
+
+		Stream<String> streamOfList2 = Stream.of("one", "two", "three", "four", "five");
+		String[] array = {"one", "one", "one", "two", "three", "four", "five"};
+		Arrays.stream(array);
+
+
+		Stream<Integer> integerStream = Stream.iterate(0, n -> n + 2).limit(5); // 0, 2, 4, 6, 8, 10,
+		Stream<String> generatedStream = Stream.generate(()-> "test").limit(5); // "test", "test", "test", "test", "test"
+
+		IntStream range = IntStream.range(1, 20);
+	}
+
+	private static List<String> getList(){
+		return List.of("one", "one", "one", "two", "three", "four", "five");
+	}
+
+
 
 }
